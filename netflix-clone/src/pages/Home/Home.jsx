94b340b02ea3ace7f2b2b7f2bd1b1b4d7ee0a52c
Negede <negedetekleyes@gmail.com>
2025-08-19 23:30:03 +0300

    banner page

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../../components/header/Header'
 import Footer from '../../components/footer/Footer'
+import Banner from '../../components/Banner/Banner'
 
 const Home = () => {
   return (
     <>
     <Header />
+    <Banner />
     <Footer />
     </>
   )
